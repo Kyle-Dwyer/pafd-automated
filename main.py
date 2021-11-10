2b5c0daca7b9ce464b3b07ed2ee50729d5b7434d
Kyle-Dwyer <34733051+Kyle-Dwyer@users.noreply.github.com>
2021-11-10 23:47:11 +0800

    Update main.py

--- main.py
+++ main.py
@@ -127,6 +127,7 @@ class Fudan:
                   "\n***********************\n")
         else:
             print("◉登录失败，请检查账号信息")
+            notify("登录失败，请检查账号信息")
             self.close()
 
     def logout(self):
@@ -310,7 +311,7 @@ if __name__ == '__main__':
         print("\n\n◉◉推送")
         count, des = daily_fudan.checkin()
         if count >= 0:
-            notify("提交成功，识别次数：{}，地址：{}，".format(address, count), des)
+            notify("提交成功，次数：{}，地址：{}，".format(count, address), des)
         else:
-            notify("提交失败，识别次数：{}".format(10), des)
+            notify("提交失败，次数：{}".format(10), des)
     daily_fudan.close()
